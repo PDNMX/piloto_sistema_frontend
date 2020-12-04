088f34a46ec7f4314fefa1966a48da8804404481
franciscoramirezvalerio <61480638+franciscoramirezvalerio@users.noreply.github.com>
2020-12-04 13:45:42 -0600

    Esquema proovedor
    Esquema proovedor

---
+++ src/server/schemas/model.proovedor.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const userSchema = new Schema({
+    dependencia:String,
+    fechaAlta:String,
+    fechaBaja:String,
+    fechaActualizacion:String,
+    estatus:String,
+    sistemas: { type: [], default: void 0 }
+});
+
+userSchema.plugin(mongoosePaginate);
+
+let Proovedor = model('Proovedores', userSchema, 'proovedores');
+
+module.exports = Proovedor;
