3759205dd92a8a50b9adb72afe1b956456978de8
Francisco Ramirez <framirez@txmglobal.com>
2021-02-24 17:35:50 -0600

    Reset Password List User
    Reset Password List User

--- src/app/components/users/listUser.tsx
+++ src/app/components/users/listUser.tsx
@@ -29,7 +29,9 @@ import {createStyles, Theme, withStyles} from '@material-ui/core/styles';
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import {alertActions} from "../../_actions/alert.actions";
 import {history} from "../../store/history";
-
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import EnhancedEncryptionIcon from '@material-ui/icons/EnhancedEncryption';
+import {requestResetPassword} from "../../store/mutations";
 
 
 
@@ -49,7 +51,8 @@ export const ListUser = () => {
     const [openModalUserInfo, setOpenModalUserInfo] = React.useState(false);
     const [selectedUser, setSelectedUser] = React.useState({_id : "",cargo: "" , correoElectronico : "",  telefono : "" ,  extension : "" , usuario : "" , sistemas : [] , fechaAlta : "" , vigenciaContrasena :""  });
     const sistemas = {S2: "Sistema de Servidores Públicos que Intervienen en Procedimientos de Contratación", S3S : "Sistema de los Servidores Públicos Sancionados", S3P : "Sistema de los Particulares Sancionados"}
-
+    const [openPassword, setOpenPassword] = React.useState(false);
+    const [usuarioCorreo, setUsuarioCorreo]= React.useState("");
 
     const renderSelect = (user) => {
         let c1= false;
@@ -81,6 +84,7 @@ export const ListUser = () => {
 
     const handleClose = () => {
         setOpen(false);
+        setOpenPassword(false);
     };
 
     const handleCloseSnackbar = () => {
@@ -116,6 +120,16 @@ export const ListUser = () => {
         handleClose();
     }
 
+    const confirmActionPassword = (correoElectronico) => {
+        alerta.estatus=false;
+        console.log(correoElectronico);
+        let data=[];
+        data["correo"]=correoElectronico;
+        data["sistema"]=true;
+        dispatch(requestResetPassword(data));
+        handleClose();
+    }
+
     const StyledTableCell = withStyles({
         root: {
             color: '#666666'
@@ -132,6 +146,14 @@ export const ListUser = () => {
         page: PropTypes.number.isRequired,
         rowsPerPage: PropTypes.number.isRequired
     };
+
+    const handleOpenModalUserPassword = (id,name,primerApellido,segundoApellido, correoElectronico) => {
+        setOpenPassword(true);
+        setNombreUsuario(name+ " "+ primerApellido+ " "+ segundoApellido);
+        setUsuarioCorreo(correoElectronico);
+    };
+
+
 
 
     const useStyles = makeStyles((theme: Theme) =>
@@ -259,6 +281,28 @@ export const ListUser = () => {
                         </Button>
                     </DialogActions>
                 </Dialog>
+
+               <Dialog
+                   open={openPassword}
+                   onClose={handleClose}
+                   aria-labelledby="alert-dialog-title"
+                   aria-describedby="alert-dialog-description"
+               >
+                   <DialogTitle id="alert-dialog-title">{"¿Deseas reestablecer la contraseña de "+ nombreUsuario+"?"}</DialogTitle>
+                   <DialogContent>
+                       <DialogContentText id="alert-dialog-description">
+                           La contraseña se generará de manera automática y se enviará a su correo electrónico.
+                       </DialogContentText>
+                   </DialogContent>
+                   <DialogActions>
+                       <Button onClick={handleClose} color="primary">
+                           Cancelar
+                       </Button>
+                       <Button onClick={()=> {confirmActionPassword(usuarioCorreo)}} color="primary" autoFocus>
+                           Aceptar
+                       </Button>
+                   </DialogActions>
+               </Dialog>
             <Grid container >
 
                 <TableContainer  component={Paper}>
@@ -288,7 +332,7 @@ export const ListUser = () => {
 
                                     {renderSelect(user)}
 
-                                    <StyledTableCell  style={{ width: 160 }} align="center">
+                                    <StyledTableCell  style={{ width: 100 }} align="center">
                                         {user.estatus? "Vigente" : "No vigente"}
                                     </StyledTableCell>
                                     <StyledTableCell style={{ width: 230 }} align="center">
@@ -302,6 +346,13 @@ export const ListUser = () => {
                                         <Button onClick={ () => redirectToRoute(`/usuario/editar/${user._id}`)}>
                                             <Tooltip title="Editar usuario" placement="top">
                                                 <Button   style={{ color: 'gray' }} ><EditOutlinedIcon/></Button>
+                                            </Tooltip>
+                                        </Button>
+                                        <Button onClick={() => handleOpenModalUserPassword(user._id, user.nombre,user.apellidoUno,user.apellidoDos, user.correoElectronico)}>
+                                            <Tooltip title="Reestablecer contraseña" placement="top">
+                                                <IconButton aria-label="expand row" size="small" >
+                                                    <EnhancedEncryptionIcon/>
+                                                </IconButton>
                                             </Tooltip>
                                         </Button>
                                         <Tooltip title="Eliminar usuario" placement="right">
