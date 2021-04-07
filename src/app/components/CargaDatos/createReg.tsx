ffc1f4669880f896aedc86247071ab5d39ce35aa
ecamargo-eng <ecamargo@txmglobal.com>
2021-04-07 17:16:26 -0500

    issues S3S S3P

--- src/app/components/CargaDatos/createReg.tsx
+++ src/app/components/CargaDatos/createReg.tsx
@@ -70,7 +70,7 @@ function MyForm(props: MyFormProps ) {
     const schema = Yup.object().shape({
         ejercicioFiscal: Yup.string().matches(new RegExp('^[0-9]{4}$'),'Debe tener 4 dígitos'),
         ramo: Yup.string(),
-        rfc: Yup.string().matches(new RegExp("^['A-z-0-9\/ ]{1,13}$"),'No se permiten puntos ,apóstrofes ni cadenas vacías máximo 13 caracteres').trim(),
+        rfc: Yup.string().matches(new RegExp("[A-ZÑ&]{3,4}[0-9]{6}[A-V1-9][A-Z1-9][0-9A]"),'No se permiten puntos ,apóstrofes ni cadenas vacías máximo 13 caracteres').trim(),
         curp: Yup.string().matches(new RegExp("[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])" +
             "(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT" +
             "|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$"), "Introducir un CURP valido"),
