d886981a03dd6ec1aa2bd53a44fdc7ca52355777
IselaMorquecho <isela.escom@gmail.com>
2021-05-04 21:11:52 -0500

    Apariencia tabla bitácora

--- src/app/components/Bitacora/ListBitacora.tsx
+++ src/app/components/Bitacora/ListBitacora.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import {connect, useDispatch, useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {
     Table,
     TableBody,
@@ -10,53 +10,41 @@ import {
     TablePagination,
     TableFooter,
     Tooltip,
-    makeStyles, Button, TableHead, ButtonGroup, Grid, IconButton, Modal, Typography
+    makeStyles, Button, TableHead, Grid, Typography
 } from "@material-ui/core";
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-import {providerActions} from "../../_actions/provider.action";
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import TablePaginationActions from "@material-ui/core/TablePagination/TablePaginationActions";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import {Alert} from "@material-ui/lab";
 import {history} from "../../store/history";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import {withStyles} from "@material-ui/core/styles";
-import {userActions} from "../../_actions/user.action";
-import {bitacoraActions} from "../../_actions/bitacora.action";
-import { CSVLink, CSVDownload } from "react-csv";
+import {CSVLink} from "react-csv";
 
 
 export const ListBitacora = () => {
 
-    const {providers,alerta,providerSelect, idUser, bitacora} = useSelector(state => ({
+    const {providers, alerta, providerSelect, idUser, bitacora} = useSelector(state => ({
         providers: state.providers,
-        alerta : state.alert,
-        providerSelect : state.providerSelect,
+        alerta: state.alert,
+        providerSelect: state.providerSelect,
         idUser: state.userInSession,
-        bitacora:state.bitacora
+        bitacora: state.bitacora
     }));
     const dispatch = useDispatch();
-    const [pagination, setPagination] =  React.useState({page : 0 , pageSize : 10 });
+    const [pagination, setPagination] = React.useState({page: 0, pageSize: 10});
 
     const handleChangePage = (event, newPage) => {
-        setPagination({page : newPage , pageSize : pagination.pageSize });
+        setPagination({page: newPage, pageSize: pagination.pageSize});
         //dispatch(userActions.requestPerPage({page : newPage ,pageSize: pagination.pageSize}));
     };
 
     const handleChangeRowsPerPage = (event) => {
-        let newSize= parseInt(event.target.value, 10);
-        if(pagination.page * newSize > bitacora.length){
-            setPagination({page : 0 , pageSize : parseInt(event.target.value, 10) });
-        }else{
-            setPagination({page : pagination.page , pageSize : parseInt(event.target.value, 10) });
+        let newSize = parseInt(event.target.value, 10);
+        if (pagination.page * newSize > bitacora.length) {
+            setPagination({page: 0, pageSize: parseInt(event.target.value, 10)});
+        } else {
+            setPagination({page: pagination.page, pageSize: parseInt(event.target.value, 10)});
         }
     };
 
@@ -67,7 +55,7 @@ export const ListBitacora = () => {
         }
     })(TableCell);
 
-    const redirectToRoute = (path) =>{
+    const redirectToRoute = (path) => {
         history.push(path);
     }
 
@@ -80,31 +68,31 @@ export const ListBitacora = () => {
     };
 
     const useStyles = makeStyles((theme) => ({
-        fontblack:{
+        fontblack: {
             color: '#666666'
         },
-        boton:{
-            marginTop:'16px',
-            marginLeft:'16px',
-            marginRight:'16px',
-            marginBottom:'12px',
-            backgroundColor:'#ffe01b',
+        boton: {
+            marginTop: '16px',
+            marginLeft: '16px',
+            marginRight: '16px',
+            marginBottom: '12px',
+            backgroundColor: '#ffe01b',
             color: '#666666'
         },
-        boton2:{
-            marginTop:'16px',
-            marginLeft:'16px',
-            marginRight:'-10px',
-            marginBottom:'12px',
-            backgroundColor:'#ffe01b',
+        boton2: {
+            marginTop: '16px',
+            marginLeft: '16px',
+            marginRight: '-10px',
+            marginBottom: '12px',
+            backgroundColor: '#ffe01b',
             color: '#666666'
         },
         gridpadding: {
             padding: '10px',
         },
-        marginright:{
+        marginright: {
             marginRight: '30px',
-            backgroundColor:'#ffe01b',
+            backgroundColor: '#ffe01b',
             color: '#666666',
             marginBottom: '30px'
         },
@@ -119,14 +107,24 @@ export const ListBitacora = () => {
             boxShadow: theme.shadows[5],
             padding: theme.spacing(2, 4, 3),
         },
+        tableHead: {
+            backgroundColor: '#34b3eb'
+        },
+        tableHeaderColumn: {
+            color: '#ffff'
+        },
+
+        whiteStyle: {
+            color: '#ffff'
+        },
     }));
 
     const headers = [
-        { label: "Tipo de Operación", key: "tipo" },
-        { label: "Fecha", key: "fecha" },
-        { label: "Sistema", key: "sistema_label" },
-        { label: "Número Registros", key: "numeroRegistros" },
-        { label: "Usuario", key: "nombre" }
+        {label: "Tipo de operación", key: "tipo"},
+        {label: "Fecha", key: "fecha"},
+        {label: "Sistema", key: "sistema_label"},
+        {label: "Número registros", key: "numeroRegistros"},
+        {label: "Usuario", key: "nombre"}
     ];
 
     const data = bitacora;
@@ -142,98 +140,104 @@ export const ListBitacora = () => {
             </Grid>
             {alerta.status == true && <Alert severity={alerta.type}>{alerta.message}</Alert>}
 
-            <Grid className= {classes.gridpadding} spacing={3} container >
-                <TableContainer component={Paper} >
+            <Grid className={classes.gridpadding} spacing={3} container>
+                <TableContainer component={Paper}>
                     {bitacora.length > 0 ?
                         <Table aria-label="custom pagination table">
-                        <TableHead >
-                            <TableRow >
-                                <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">Operación</TableCell>
-                                <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">Fecha</TableCell>
-                                <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">Sistema</TableCell>
-                                <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">Registros</TableCell>
-                                <TableCell className={classes.fontblack} align="center">Usuario</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody key="bitacora">
-                            {bitacora.slice(pagination.page * pagination.pageSize, pagination.page * pagination.pageSize + pagination.pageSize).map((rowbitacora)  => (
-
-                                <TableRow key={rowbitacora._id}>
-                                    <TableCell className={classes.fontblack} component="th" scope="row" style={{ width: 'auto'}} align="center">
-                                        {rowbitacora.tipo}
-                                    </TableCell>
-                                    <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">
-                                        {rowbitacora.fecha}
-                                    </TableCell>
-                                    <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="left">
-                                        <div>
-                                            {rowbitacora.sistema_label}
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">
-                                        {rowbitacora.numeroRegistros}
-                                    </TableCell>
-                                    <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">
-                                        {rowbitacora.nombre}
-                                    </TableCell>
+                            <TableHead className={classes.tableHead}>
+                                <TableRow>
+                                    <TableCell className={classes.tableHeaderColumn} style={{width: 'auto'}}
+                                               align="center"><b>Operación</b></TableCell>
+                                    <TableCell className={classes.tableHeaderColumn} style={{width: 'auto'}}
+                                               align="left"><b>Fecha</b></TableCell>
+                                    <TableCell className={classes.tableHeaderColumn} style={{width: 'auto'}}
+                                               align="left"><b>Sistema</b></TableCell>
+                                    <TableCell className={classes.tableHeaderColumn} style={{width: 'auto'}}
+                                               align="center"><b>Registros</b></TableCell>
+                                    <TableCell className={classes.tableHeaderColumn} align="center"><b>Usuario</b></TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                { pagination.pageSize != undefined  && pagination.page != undefined  && <TablePagination
-                                    rowsPerPageOptions={[3,5, 10, 25, { label: 'Todos', value: -1 }]}
-                                    colSpan={6}
-                                    count={bitacora.length}
-                                    rowsPerPage={pagination.pageSize}
-                                    page={pagination.page}
-                                    SelectProps={{
-                                        inputProps: { 'aria-label': 'Registros por página' },
-                                        native: true,
-                                    }}
-                                    onChangePage={handleChangePage}
-                                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                                    ActionsComponent={TablePaginationActions}
-                                />}
-                            </TableRow>
-                        </TableFooter>
-                    </Table>
-                    :
+                            </TableHead>
+                            <TableBody key="bitacora">
+                                {bitacora.slice(pagination.page * pagination.pageSize, pagination.page * pagination.pageSize + pagination.pageSize).map((rowbitacora) => (
+
+                                    <TableRow key={rowbitacora._id}>
+                                        <TableCell className={classes.fontblack} component="th" scope="row"
+                                                   style={{width: 'auto'}} align="center">
+                                            {rowbitacora.tipo}
+                                        </TableCell>
+                                        <TableCell className={classes.fontblack} style={{width: 'auto'}} align="left">
+                                            {rowbitacora.fecha}
+                                        </TableCell>
+                                        <TableCell className={classes.fontblack} style={{width: 'auto'}} align="left">
+                                            <div>
+                                                {rowbitacora.sistema_label}
+                                            </div>
+                                        </TableCell>
+                                        <TableCell className={classes.fontblack} style={{width: 'auto'}} align="center">
+                                            {rowbitacora.numeroRegistros}
+                                        </TableCell>
+                                        <TableCell className={classes.fontblack} style={{width: 'auto'}} align="center">
+                                            {rowbitacora.nombre}
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    {pagination.pageSize != undefined && pagination.page != undefined &&
+                                    <TablePagination
+                                        rowsPerPageOptions={[3, 5, 10, 25, {label: 'Todos', value: -1}]}
+                                        colSpan={6}
+                                        count={bitacora.length}
+                                        rowsPerPage={pagination.pageSize}
+                                        page={pagination.page}
+                                        SelectProps={{
+                                            inputProps: {'aria-label': 'Registros por página'},
+                                            native: true,
+                                        }}
+                                        onChangePage={handleChangePage}
+                                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                                        ActionsComponent={TablePaginationActions}
+                                    />}
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                        :
                         <Table>
                             <TableRow>
-                                <TableCell className={classes.fontblack} style={{ width: 'auto' }} align="center">
+                                <TableCell className={classes.fontblack} style={{width: 'auto'}} align="center">
                                     No hay registros para está consulta.
                                 </TableCell>
                             </TableRow>
                         </Table>}
                 </TableContainer>
             </Grid>
-                <Grid  spacing={3} justify="flex-end"
-                       alignItems="flex-end"
-                       container
-                       item
-                       xs={12}
-                       md={12}>
-                    <Tooltip title="Reporte Nuevo" placement="left">
-                        <Button  onClick={ () => redirectToRoute("/bitacora")}
-                                 variant="contained"
-                                 className={classes.boton}
-                                 endIcon={<AddBoxIcon>Reporte Nuevo</AddBoxIcon>}
-                        >
-                            Reporte Nuevo
-                        </Button>
-                    </Tooltip>
-                    <Tooltip title="Descargar CSV" placement="right">
-                        <CSVLink data={data} headers={headers} filename={"Bitacora.csv"}>
+            <Grid spacing={3} justify="flex-end"
+                  alignItems="flex-end"
+                  container
+                  item
+                  xs={12}
+                  md={12}>
+                <Tooltip title="Reporte Nuevo" placement="left">
+                    <Button onClick={() => redirectToRoute("/bitacora")}
+                            variant="contained"
+                            className={classes.boton}
+                            endIcon={<AddBoxIcon>Reporte Nuevo</AddBoxIcon>}
+                    >
+                        Reporte Nuevo
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Descargar CSV" placement="right">
+                    <CSVLink data={data} headers={headers} filename={"Bitacora.csv"}>
                         <Button
-                                 variant="contained"
-                                 className={classes.boton2}
-                                 endIcon={<FileCopyIcon>Descargar CSV</FileCopyIcon>}
+                            variant="contained"
+                            className={classes.boton2}
+                            endIcon={<FileCopyIcon>Descargar CSV</FileCopyIcon>}
                         >
                             Descargar CSV
                         </Button>
-                        </CSVLink>
-                    </Tooltip>
+                    </CSVLink>
+                </Tooltip>
             </Grid>
         </div>
     );
