0ebab60a786bfdf25187c4406618d216721126f8
Sergio Rodriguez <sergio.rdzsg@gmail.com>
2021-06-29 17:54:35 -0500

    nuevos archivos al gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         extensions: ['.js','.jsx','.tsx','.json']
     },
     devServer: {
-        host:'localhost',
+        host:'0.0.0.0',
         port: 3000,
         historyApiFallback: true
     },
